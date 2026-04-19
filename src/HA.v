d9282dbaa1611d0ae939100d5e1554bd32e71eb3
Radin <radin44@protonmail.com>
2026-04-19 13:31:38 -0700

    fixed repetition

--- src/HA.v
+++ src/HA.v
@@ -1,26 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/06/2026 02:21:15 PM
-// Design Name: 
-// Module Name: HA
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
 
 module HA (
     input  A,
